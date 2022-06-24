205ae0247bcd7fd90bf8f12b517a83bed2e78576
Harry <hvolker@qa.com>
2022-06-24 09:41:21 +0000

    parameterising the container port

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ app_name=lbg-api-app
 dockerhub_username=htrvolker
 tag=latest
 image_name=${dockerhub_username}/${app_name}:${tag}
+container_port=5000
 
 # build the docker image
 echo "Building image:"
@@ -24,5 +25,5 @@ echo
 
 # running the container published to port 80 on the host machine
 echo "Running new container:"
-docker run -d -p 80:8080 --name $app_name $image_name
+docker run -d -p 80:$container_port -e PORT=$container_port --name $app_name $image_name
 echo
