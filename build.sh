e98f1493d33b0ba86f8076709b6ba48b4de8535e
Harry <hvolker@qa.com>
2022-06-24 09:14:11 +0000

    added build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set variables
+app_name=lbg-api-app
+dockerhub_username=htrvolker
+tag=latest
+image_name=${dockerhub_username}/${app_name}:${tag}
+
+# build the docker image
+echo "Building image:"
+docker build -t $image_name .
+echo
+
+# push image to dockerhub
+echo "Pushing image:"
+docker push $image_name
+echo
+
+# stop and remove running instance of the app
+echo "Removing running container:"
+docker stop $app_name
+docker rm $app_name
+echo
+
+# running the container published to port 80 on the host machine
+echo "Running new container:"
+docker run -d -p 80:8080 --name $app_name $image_name
+echo
